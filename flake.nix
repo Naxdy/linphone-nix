57628bea7d533c25875f4d0c227f950a9c36cbcb
Naxdy <naxdy@naxdy.org>
2025-09-15 12:49:12 +0200

    bc-ispell: init

--- flake.nix
+++ flake.nix
@@ -45,6 +45,7 @@
             lime
             bc-soci
             liblinphone
+            bc-ispell
             linphone-desktop
             ;
         }
---
+++ packages/bc-ispell/default.nix
@@ -0,0 +1,43 @@
+{
+  cmake,
+  fetchFromGitLab,
+  lib,
+  stdenv,
+}:
+stdenv.mkDerivation {
+  pname = "bc-ispell";
+  version = "unstable-2025-05-05";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.linphone.org";
+    group = "BC";
+    owner = "public/external";
+    repo = "ispell";
+    rev = "05574fe160222c3d0b6283c1433c9b087271fad1";
+    sha256 = "sha256-YoRLiMjk2BxoI27xc2nzucxfHV9UbouFRSECb3RdHGo=";
+  };
+
+  patches = [
+    ./install-config-files.patch
+  ];
+
+  cmakeFlags = [
+    # Do not build static libraries
+    "-DENABLE_STATIC=NO"
+    "-DBUILD_SHARED_LIBS=ON"
+  ];
+
+  preConfigure = ''
+    substituteInPlace CMakeLists.txt --replace-fail \
+      LibraryDepends Targets
+  '';
+
+  nativeBuildInputs = [ cmake ];
+
+  meta = with lib; {
+    description = "Spell Checker";
+    homepage = "https://gitlab.linphone.org/BC/public/external/ispell";
+    platforms = platforms.all;
+    maintainers = with maintainers; [ naxdy ];
+  };
+}
